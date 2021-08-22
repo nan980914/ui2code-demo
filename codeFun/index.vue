7cea86cdf132d59efc7f731a9fb750a999e7b0aa
linan31 <linan31@58.com>
2021-08-22 20:30:10 +0800

    feat: Compare three ui2code tools

---
+++ codeFun/index.vue
@@ -0,0 +1,281 @@
+<template>
+  <div class="flex-col container">
+    <div class="flex-col header">
+      <div class="justify-center group">
+        <div class="flex-row group_1">
+          <img
+            src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674460495736697.png"
+            class="image"
+          />
+          <img
+            src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674460472402413.png"
+            class="image_1"
+          />
+        </div>
+        <span class="text">9:53 AM</span>
+        <div class="flex-row group_2">
+          <img
+            src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674460446684045.png"
+            class="image_2"
+          />
+          <span class="text_1">100%</span>
+          <img
+            src="https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16292159862934909493.png"
+            class="image_3"
+          />
+        </div>
+      </div>
+      <div class="justify-center group_3">
+        <span>订单中心</span>
+        <div class="flex-row section_1">
+          <div class="left-image-wrapper flex-col items-center view">
+            <img
+              src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674460406269510.png"
+              class="image_4"
+            />
+          </div>
+          <div class="section_2"></div>
+          <div class="left-image-wrapper flex-col items-center view_1">
+            <img
+              src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674460364921877.png"
+              class="image_5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex-col group_4">
+      <div class="flex-col section_3">
+        <div class="flex-row section_4">
+          <img
+            src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674472947416676.png"
+            class="image_6"
+          />
+          <span class="text_3">您有1个新的待处理订单，为空调清洗服务项…</span>
+          <img
+            src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674473232232618.png"
+            class="image_7"
+          />
+        </div>
+        <div class="flex-col group_5">
+          <div class="flex-col group_6">
+            <div class="justify-center group_7">
+              <span>今日收入（元）</span>
+              <img
+                src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674472334859877.png"
+                class="image_8"
+              />
+            </div>
+            <span class="text_5">12.00</span>
+          </div>
+          <div class="flex-col group_8">
+            <div class="divider"></div>
+            <div class="flex-row equal-division">
+              <div class="equal-division-item flex-col items-center group_9">
+                <span class="text_6">待收入（元）</span>
+                <span class="text_7 text_8">12.00</span>
+              </div>
+              <div class="horiz-divider"></div>
+              <div class="equal-division-item flex-col items-center group_10">
+                <span class="text_9">累计收入（元）</span>
+                <span class="text_7 text_10">128.00</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex-col group_11">
+        <div class="flex-col group_12">
+          <div class="flex-col section_5">
+            <div class="justify-between group_13">
+              <div class="flex-row group_14">
+                <img
+                  src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674468244119509.png"
+                  class="image_9"
+                />
+                <span class="text_11">订单管理</span>
+              </div>
+              <div class="flex-row group_15">
+                <span>全部订单</span>
+                <img
+                  src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674460512982194.png"
+                  class="image_10"
+                />
+              </div>
+            </div>
+            <div class="divider_1"></div>
+            <div class="flex-row equal-division_1">
+              <div class="equal-division-item flex-col items-center group_16">
+                <span class="text_13">待确认订单</span>
+                <span class="text_7 text_14">1</span>
+              </div>
+              <div class="horiz-divider_1"></div>
+              <div class="equal-division-item flex-col items-center group_17">
+                <span class="text_15">待处理订单</span>
+                <span class="text_7 text_16">0</span>
+              </div>
+              <div class="horiz-divider_1"></div>
+              <div class="equal-division-item flex-col items-center group_18">
+                <span class="text_17">售后订单</span>
+                <span class="text_7 text_18">1</span>
+              </div>
+            </div>
+          </div>
+          <div class="flex-col list">
+            <template v-for="(item, i) in orderList">
+              <div class="list-item flex-col" :key="'item' + i">
+                <div class="group_19 justify-between">
+                  <div class="left-group flex-row">
+                    <span>{{ item.timeTitile }}</span>
+                    <span>{{ item.time }}</span>
+                    <span class="text_21">{{ item.day }}</span>
+                  </div>
+                  <span class="text_23">{{ item.status }}</span>
+                </div>
+                <div class="group_20 justify-between">
+                  <div class="flex-row">
+                    <span class="text_25">{{ item.name }}</span>
+                    <span class="text_27">{{ item.phone }}</span>
+                    <img class="image_11" :src="item.phoneImg" />
+                  </div>
+                  <div class="right-text-wrapper flex-col items-center">
+                    <span>{{ item.type }}</span>
+                  </div>
+                </div>
+                <div class="section_9 justify-between">
+                  <div class="flex-row">
+                    <img class="image_13" :src="item.orderImg" />
+                    <div class="right-group flex-col">
+                      <div class="top-group flex-col">
+                        <span class="text_30">{{ item.orderName }}</span>
+                        <span class="text_32">{{ item.orderInfo }}</span>
+                      </div>
+                      <span class="text_34">{{ item.money }}</span>
+                    </div>
+                  </div>
+                  <span class="text_36">{{ item.number }}</span>
+                </div>
+                <div class="group_23">
+                  <span class="text_38">{{ item.total }}</span>
+                  <span class="text_40">{{ item.actural }}</span>
+                </div>
+                <div class="group_24 justify-end">
+                  <div class="left-text-wrapper flex-col items-center">
+                    <span>{{ item.cancel }}</span>
+                  </div>
+                  <div class="right-text-wrapper_1 flex-col items-center">
+                    <span>{{ item.confirm }}</span>
+                  </div>
+                </div>
+              </div>
+            </template>
+          </div>
+        </div>
+        <div class="justify-center button">
+          <div class="left-section"></div>
+          <span class="text_78">暂无更多信息</span>
+          <div class="left-section view_38"></div>
+        </div>
+      </div>
+    </div>
+    <div class="justify-between tab-bar">
+      <div class="left-group_1 flex-col items-center view_39">
+        <img
+          src="https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16292159865861550020.png"
+          class="image_19"
+        />
+        <span>首页</span>
+      </div>
+      <div class="left-group_1 flex-col items-center view_40">
+        <img
+          src="https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16292159865873492905.png"
+          class="image_19"
+        />
+        <span>订单</span>
+      </div>
+      <div class="left-group_1 flex-col items-center view_41">
+        <img
+          src="https://project-user-resource-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16292159865876616041.png"
+          class="image_19"
+        />
+        <span>我的</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      orderList: [
+        {
+          timeTitile: "服务时间：",
+          time: "2020/08/12",
+          day: "上午",
+          status: "服务中",
+          name: "刘美丽",
+          phone: "15032547844",
+          phoneImg:
+            "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674471376503557.png",
+          type: "粉丝单",
+          orderImg:
+            "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674469281769685.png",
+          orderName: "洗衣机深度清洗",
+          orderInfo: "滚桶洗衣机全拆洗(带烘干)",
+          money: "¥199.00/台",
+          number: "x3",
+          total: "总计￥597.00，",
+          actural: "实付¥497.00",
+          cancel: "取消订单",
+          confirm: "完成服务",
+        },
+        {
+          timeTitile: "服务时间：",
+          time: "2020/08/12",
+          day: "13:00-13:30",
+          status: "售后中",
+          name: "刘美丽",
+          phone: "15032547844",
+          phoneImg:
+            "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674471376503557.png",
+          type: "公海单",
+          orderImg:
+            "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674469281769685.png",
+          orderName: "洗衣机深度清洗",
+          orderInfo: "滚桶洗衣机全拆洗(带烘干)",
+          money: "¥199.00/台",
+          number: "x3",
+          total: "售后订单",
+          actural: null,
+          cancel: "取消订单",
+          confirm: "完成售后",
+        },
+        {
+          timeTitile: "服务时间：",
+          time: "2020/08/12",
+          day: "13:00-13:30",
+          status: "售后中",
+          name: "刘美丽",
+          phone: "15032547844",
+          phoneImg:
+            "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674471376503557.png",
+          type: "公海单",
+          orderImg:
+            "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/611924cd6b68fa00119f30a0/611a11c16b68fa00119f4e9f/16293674469281769685.png",
+          orderName: "洗衣机深度清洗",
+          orderInfo: "滚桶洗衣机全拆洗(带烘干)",
+          money: "¥199.00/台",
+          number: "x3",
+          total: "售后订单",
+          actural: null,
+          cancel: "取消订单",
+          confirm: "完成售后",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style src="./index.css" />
---
+++ imgCook/index.vue
@@ -0,0 +1,392 @@
+<template>
+  <div class="page">
+    <div class="block">
+      <div class="header">
+        <img
+          class="icon-qrcode"
+          src="./images/img_90591_0_3.png"
+        />
+        <span class="tag">9:53 AM</span>
+        <div class="container-inner">
+          <img
+            class="icon-right"
+            src="./images/img_90591_0_2.png"
+          />
+          <span class="percent">100%</span>
+          <div class="wrapper"><div class="horizontal-line" /></div>
+          <img
+            class="dot"
+            src="./images/img_90591_0_1.png"
+          />
+        </div>
+      </div>
+      <div class="body">
+        <span class="label">订单中心</span>
+        <div class="container-inner-1">
+          <img
+            class="large-icon"
+            src="./images/img_90591_0_0.png"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="tabbar">
+      <div class="primary">
+        <div class="container-inner-2">
+          <div class="wrapper-1">
+            <div class="wrapper-inner">
+              <div class="group">
+                <div class="icon-horn-wrapper">
+                  <img
+                    class="icon-horn"
+                    src="./images/img_90591_0_29.png"
+                  />
+                </div>
+                <span class="desc"
+                  >您有1个新的待处理订单，为空调清洗服务项…</span
+                >
+              </div>
+              <div class="icon-right-wrapper">
+                <img
+                  class="icon-right-1"
+                  src="./images/img_90591_0_30.png"
+                />
+              </div>
+            </div>
+            <div class="wrapper-inner-1">
+              <span class="word">今日收入（元）</span>
+              <div class="group-1">
+                <img
+                  class="dot-1"
+                  src="./images/img_90591_0_28.png"
+                />
+              </div>
+            </div>
+            <span class="count">12.00</span>
+          </div>
+          <div class="wrapper-2">
+            <img
+              class="bg"
+              src="./images/img_90591_0_27.png"
+            />
+            <div class="wrapper-inner-2">
+              <div class="group-2">
+                <div class="view">
+                  <span class="tag-1">待收入（元）</span>
+                  <span class="num">12.00</span>
+                </div>
+              </div>
+              <div class="group-3">
+                <img
+                  class="vertical-line"
+                  src="./images/img_90591_0_26.png"
+                />
+                <div class="view-1">
+                  <span class="label-1">累计收入（元）</span>
+                  <span class="count-1">128.00</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="container-inner-3">
+          <div class="icon-right-wrapper-1">
+            <img
+              class="icon-right-2"
+              src="./images/img_90591_0_6.png"
+            />
+          </div>
+          <div class="wrapper-3">
+            <img
+              class="background"
+              src="./images/img_90591_0_7.png"
+            />
+            <div class="wrapper-inner-3">
+              <div class="group-4">
+                <div class="view-2">
+                  <span class="word-1">待确认订单</span>
+                  <span class="num-1">1</span>
+                </div>
+              </div>
+              <div class="group-5">
+                <img
+                  class="vertical-line-1"
+                  src="./images/img_90591_0_4.png"
+                />
+                <div class="view-3">
+                  <span class="tag-2">待处理订单</span>
+                  <span class="num-2">0</span>
+                </div>
+              </div>
+              <div class="group-6">
+                <img
+                  class="vertical-line-2"
+                  src="./images/img_90591_0_5.png"
+                />
+                <div class="view-4">
+                  <span class="label-2">售后订单</span>
+                  <span class="num-3">1</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <span class="word-2">订单管理</span> <span class="tag-3">全部订单</span>
+        <div class="container-inner-4">
+          <div class="wrapper-4">
+            <div class="wrapper-inner-4"><div class="horizontal-line-1" /></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="goods-list-1v2">
+      <img
+        class="bg-1"
+        src="./images/img_90591_0_8.png"
+      />
+      <div class="container-1">
+        <div class="container-inner-5">
+          <div class="wrapper-5">
+            <div class="wrapper-inner-5">
+              <span class="label-3">服务时间：</span>
+              <span class="time">2020/08/12 上午</span>
+            </div>
+            <span class="word-3">服务中</span>
+          </div>
+        </div>
+        <div class="container-inner-6">
+          <img
+            class="background-1"
+            src="./images/img_90591_0_22.png"
+          />
+          <div class="wrapper-6">
+            <div class="wrapper-inner-6">
+              <span class="nick-name">刘美丽</span>
+              <span class="phone">15032547844</span>
+              <div class="icon-home-wrapper">
+                <img
+                  class="icon-home"
+                  src="./images/img_90591_0_25.png"
+                />
+              </div>
+            </div>
+            <div class="tag-wrapper"><span class="tag-4">粉丝单</span></div>
+          </div>
+          <div class="wrapper-7">
+            <div class="wrapper-inner-7">
+              <div class="group-7">
+                <img
+                  class="large-icon-1"
+                  src="./images/img_90591_0_24.png"
+                />
+              </div>
+              <div class="price-wrapper">
+                <span class="info">洗衣机深度清洗</span>
+                <span class="item-title">滚桶洗衣机全拆洗(带烘干)</span>
+                <span class="price">¥199.00/台</span>
+              </div>
+            </div>
+            <span class="tag-5">x3</span>
+          </div>
+          <div class="wrapper-8">
+            <span class="money">总计￥597.00，</span>
+            <span class="price-1">实付¥497.00</span>
+          </div>
+        </div>
+        <div class="container-inner-7">
+          <img
+            class="bg-2"
+            src="./images/img_90591_0_23.png"
+          />
+          <div class="grid">
+            <div class="col-i0"><span class="cancel">取消订单</span></div>
+            <div class="col-i1"><span class="tag-6">完成服务</span></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="goods-list-1v2-i0">
+      <div class="header-1">
+        <div class="container-inner-8">
+          <div class="wrapper-9">
+            <span class="label-4">服务时间：</span>
+            <span class="timer">2020/08/12 13:00-13:30</span>
+          </div>
+          <span class="word-4">售后中</span>
+        </div>
+      </div>
+      <div class="body-1">
+        <img
+          class="background-2"
+          src="./images/img_90591_0_13.png"
+        />
+        <div class="container-inner-9">
+          <div class="wrapper-10">
+            <span class="name">刘美丽</span>
+            <span class="phone-num">15032547844</span>
+            <div class="icon-home-wrapper-1">
+              <img
+                class="icon-home-1"
+                src="./images/img_90591_0_15.png"
+              />
+            </div>
+          </div>
+          <div class="tag-wrapper-1"><span class="tag-7">公海单</span></div>
+        </div>
+        <div class="container-inner-10">
+          <div class="wrapper-11">
+            <div class="wrapper-inner-8">
+              <img
+                class="large-icon-2"
+                src="./images/img_90591_0_12.png"
+              />
+            </div>
+            <div class="money-wrapper">
+              <span class="meta">洗衣机深度清洗</span>
+              <span class="item-title-1">滚桶洗衣机全拆洗(带烘干)</span>
+              <span class="money-1">¥199.00/台</span>
+            </div>
+          </div>
+          <span class="tag-8">x3</span>
+        </div>
+        <div class="container-inner-11">
+          <span class="label-5">售后订单</span>
+          <div class="wrapper-12">
+            <span class="price-2">总计￥597.00，</span>
+            <span class="money-2">实付¥497.00</span>
+          </div>
+        </div>
+      </div>
+      <div class="footer">
+        <img
+          class="bg-3"
+          src="./images/img_90591_0_14.png"
+        />
+        <div class="grid-1">
+          <div class="col-i0-1"><span class="cancel-1">取消订单</span></div>
+          <div class="col-i1-1"><span class="tag-9">完成售后</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="goods-list-1v2-i1">
+      <div class="header-2">
+        <div class="container-inner-12">
+          <div class="wrapper-13">
+            <span class="word-5">服务时间：</span>
+            <span class="clock">2020/08/12 13:00-13:30</span>
+          </div>
+          <span class="tag-10">售后中</span>
+        </div>
+      </div>
+      <div class="body-2">
+        <img
+          class="background-3"
+          src="./images/img_90591_0_17.png"
+        />
+        <div class="container-inner-13">
+          <div class="wrapper-14">
+            <span class="user">刘美丽</span>
+            <span class="phone-1">15032547844</span>
+            <div class="icon-home-wrapper-2">
+              <img
+                class="icon-home-2"
+                src="./images/img_90591_0_19.png"
+              />
+            </div>
+          </div>
+          <div class="tag-wrapper-2"><span class="tag-11">公海单</span></div>
+        </div>
+        <div class="container-inner-14">
+          <div class="wrapper-15">
+            <div class="wrapper-inner-9">
+              <img
+                class="large-icon-3"
+                src="./images/img_90591_0_16.png"
+              />
+            </div>
+            <div class="price-wrapper-1">
+              <span class="subtitle">洗衣机深度清洗</span>
+              <span class="item-title-2">滚桶洗衣机全拆洗(带烘干)</span>
+              <span class="price-3">¥199.00/台</span>
+            </div>
+          </div>
+          <span class="label-6">x3</span>
+        </div>
+        <div class="container-inner-15">
+          <span class="word-6">售后订单</span>
+          <div class="wrapper-16">
+            <span class="money-3">总计￥597.00，</span>
+            <span class="price-4">实付¥497.00</span>
+          </div>
+        </div>
+      </div>
+      <div class="footer-1">
+        <img
+          class="bg-4"
+          src="./images/img_90591_0_18.png"
+        />
+        <div class="grid-2">
+          <div class="col-i0-2"><span class="cancel-2">取消订单</span></div>
+          <div class="col-i1-2"><span class="tag-12">完成售后</span></div>
+        </div>
+      </div>
+    </div>
+    <div class="block-1">
+      <div class="primary-1">
+        <img
+          class="icon-list"
+          src="./images/img_90591_0_20.png"
+        />
+        <span class="info-1">暂无更多信息</span>
+      </div>
+      <div class="side">
+        <img
+          class="icon-list-1"
+          src="./images/img_90591_0_21.png"
+        />
+      </div>
+    </div>
+    <div class="tabbar-list">
+      <div class="primary-2">
+        <div class="icon-home-wrapper-3">
+          <img
+            class="icon-home-3"
+            src="./images/img_90591_0_11.png"
+          />
+        </div>
+        <span class="tag-13">首页</span>
+      </div>
+      <div class="sub-i0">
+        <div class="icon-list-wrapper">
+          <img
+            class="icon-list-2"
+            src="./images/img_90591_0_10.png"
+          />
+        </div>
+        <span class="label-7">订单</span>
+      </div>
+      <div class="sub-i1">
+        <div class="icon-mine-wrapper">
+          <img
+            class="icon-mine"
+            src="./images/img_90591_0_9.png"
+          />
+        </div>
+        <span class="word-7">我的</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      constants: {},
+    };
+  },
+  methods: {},
+};
+</script>
+<style src="./index.response.css" />
